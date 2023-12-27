585d9efcc062adf918d8c3f7cc7351e339d38ef8
amith628 <140142007+amith628@users.noreply.github.com>
2023-12-27 11:40:35 +0530

    Commit

--- lib/Login with validate.dart
+++ lib/Login with validate.dart
@@ -6,11 +6,11 @@ import 'Listview.dart';
 
 void main() {
   runApp(DevicePreview(
-      builder: (BuildContext context) => MaterialApp(useInheritedMediaQuery: true,
-        debugShowCheckedModeBanner: false,
-
-        home: Login_validate(),
-      )));
+      builder: (BuildContext context) => MaterialApp(
+            useInheritedMediaQuery: true,
+            debugShowCheckedModeBanner: false,
+            home: Login_validate(),
+          )));
 }
 
 class Login_validate extends StatefulWidget {
@@ -20,12 +20,16 @@ class Login_validate extends StatefulWidget {
 
 class Login_validate_state extends State<Login_validate> {
   @override
-  GlobalKey<FormState> formkey=GlobalKey();
-  bool showpass=true;
+  GlobalKey<FormState> formkey = GlobalKey();
+  bool showpass = true;
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login Page",style: TextStyle(fontSize: 30),),
+        title: Text(
+          "Login Page",
+          style: TextStyle(fontSize: 30),
+        ),
       ),
       body: SingleChildScrollView(
         child: Form(
@@ -46,44 +50,46 @@ class Login_validate_state extends State<Login_validate> {
                       labelText: "username",
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20))),
-                  validator:(uname) {
-                    if(uname!.isEmpty || !uname.contains('@') ||!uname.contains('.')) {
+                  validator: (uname) {
+                    if (uname!.isEmpty ||
+                        !uname.contains('@') ||
+                        !uname.contains('.')) {
                       return 'Enter valid username';
-                    }
-                    else{
+                    } else {
                       return null;
                     }
                   },
-
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 30, left: 50, right: 50),
                 child: TextFormField(
-                  obscureText:showpass ,
+                  obscureText: showpass,
                   obscuringCharacter: "*",
                   decoration: InputDecoration(
                       hintText: "password",
                       prefixIcon: Icon(Icons.person),
-                      suffixIcon: IconButton(onPressed: () {
-                        setState(() {
-                          if(showpass){
-                            showpass=false;
-                          }
-                          else{
-                            showpass=true;
-                          }
-
-                        });
-                      }, icon: Icon(showpass==true ? Icons.visibility_off : Icons.visibility),),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            if (showpass) {
+                              showpass = false;
+                            } else {
+                              showpass = true;
+                            }
+                          });
+                        },
+                        icon: Icon(showpass == true
+                            ? Icons.visibility_off
+                            : Icons.visibility),
+                      ),
                       labelText: "password",
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20))),
-                  validator:(password) {
-                    if(password!.isEmpty || password.length<6){
+                  validator: (password) {
+                    if (password!.isEmpty || password.length < 6) {
                       return "Enter a valid password";
-                    }
-                    else
+                    } else
                       return null;
                   },
                 ),
@@ -91,12 +97,15 @@ class Login_validate_state extends State<Login_validate> {
               SizedBox(
                 height: 30,
               ),
-              ElevatedButton(onPressed: () {
-                final valid =formkey.currentState!.validate();
-                if(valid){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Homepage()));
-                }
-              }, child: Text("Login")),
+              ElevatedButton(
+                  onPressed: () {
+                    final valid = formkey.currentState!.validate();
+                    if (valid) {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Homepage()));
+                    }
+                  },
+                  child: Text("Login")),
               SizedBox(
                 height: 30,
               ),
